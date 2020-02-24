9b2b0b4388def1982cc84601fb197fd5e5d0fb74
Osagie Erhabor <erhaborosagie@gmail.com>
2020-02-24 20:23:47 +0100

    SQL Connection

--- src/main/java/com/example/demo/model/Cards.java
+++ src/main/java/com/example/demo/model/Cards.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -13,7 +12,6 @@ import javax.validation.constraints.NotNull;
  */
 @Data
 @Entity
-@Table(name = "cards")
 public class Cards {
 
     @Id
