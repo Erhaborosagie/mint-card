c16bfcb92116918736b74094bd154299682f760c
Osagie Erhabor <erhaborosagie@gmail.com>
2020-02-25 02:36:54 +0100

    Created Cards Repository

---
+++ src/main/java/com/example/demo/service/dao/CardService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service.dao;
+
+import com.example.demo.model.Cards;
+import com.example.demo.repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.TransactionScoped;
+
+/**
+ * Created by Osagie Erhabor on 25/02/2020.
+ */
+@Service
+@TransactionScoped
+public class CardService {
+    private final CardsRepository cardsRepository;
+
+    @Autowired
+    public CardService(CardsRepository cardsRepository) {
+        this.cardsRepository = cardsRepository;
+    }
+
+    Cards findCardsByCardId(Long cardId){
+        return cardsRepository.findCardsByCardId(cardId);
+    }
+}
